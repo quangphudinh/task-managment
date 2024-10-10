c46ca239ea76308869e18ab272b8713449fe3581
John Doe <johndoe@email.com>
2024-10-10 17:38:52 +0700

    change-multi

--- api/v1/controllers/task.controller.js
+++ api/v1/controllers/task.controller.js
@@ -80,3 +80,37 @@ module.exports.changeStatus = async (req, res) => {
     }
 
 }
+
+//[PATCH] /api/v1/tasks/change-multi
+module.exports.changeMulti = async (req, res) => {
+    try {
+        const {ids , key , value} = req.body;
+        console.log(ids , key , value);
+
+        switch (key) {
+            case "status":
+                await Task.updateMany({
+                    _id: { $in: ids }
+                }, {
+                    status: value
+                });
+                res.json({
+                    code: 200,
+                    message: "Cập nhật trạng thái thành công"
+                })
+                break;
+            default:
+                res.json({
+                    code: 400,
+                    message: "Không tồn tại!"
+                })
+                break;
+        }
+        
+    } catch (error) {
+        res.json({
+            code: 400,
+            message: "Không tồn tại!"
+        })
+    }
+}
