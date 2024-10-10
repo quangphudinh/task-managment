ab5c42137db971cf6c0fd366c2e0730daadd434c
John Doe <johndoe@email.com>
2024-10-10 20:25:46 +0700

    Cors npm

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const express = require("express");
 require('dotenv').config();
 const database = require("./config/database");
 const bodyParser = require('body-parser')
-
+//khac phuc loi k lay dc data tu FE
+const cors = require('cors')
 const routesApiVer1 = require("./api/v1/routes/index.route");
 
 database.connect(); // ket noi database
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors())
 
 // parse application/json
 app.use(bodyParser.json())
